12a4d9960bf9f5d78e30588a20da1cd40490c294
vbcc2001 <vbcc2001@gmail.com>
2015-12-10 20:29:01 +0800

    第二版

---
+++ XXX_ISS_WEB/WebRoot/iss/js/json.js
@@ -0,0 +1,315 @@
+var c = [
+{"img":"t_1.png","check":false,"t" :"头颈部","t_en" :"Head and Neck","c" :[
+	{"check":false,"t":"意识","c" :[
+		{"check":false,"val":"5","c":"昏迷伴有不适动作；昏迷>24小时"},
+		{"check":false,"val":"4","c":"昏迷1-6小时伴神经障碍；昏迷6-24小时；仅对疼痛刺激有恰当反应"},
+		{"check":false,"val":"3","c":"昏迷1~6小时；昏迷<1小时伴有神经障碍"},
+		{"check":false,"val":"2","c":"逆行性遗忘、嗜睡、木僵、迟钝能被语言刺激唤醒、失去知觉<1小时"},
+		{"check":false,"val":"1","c":"头痛、头晕"}]
+	},
+	{"check":false,"t":"脑组织","c" :[
+		{"check":false,"val":"5","c":"脑干损伤；大脑广泛挫伤；小脑广泛挫伤；硬膜外双侧/大范围血肿；脑肿胀（脑室或脑干池消失）；小脑/大脑穿透伤/弥漫性轴索损伤"},
+		{"check":false,"val":"4","c":"脑膜破裂、脑组织外露、缺损；大脑挫伤范围30-50ml,直径>4cm，中线移位> 5cm，中度脑肿胀、脑室脑干池受压；小脑挫伤范围15-30ml，直径>3cm，硬膜外/下血肿"},
+		{"check":false,"val":"3","c":"埂塞/脑挫伤：浅表<30ml,直径<4cm,中线<5cm;小脑挫伤,轻度脑肿胀/水肿（脑室受压，无脑干池受压;蛛网膜下腔出血；脑垂体受损"},
+		{"check":false,"val":"2","c":"梗塞/挫伤；浅表，<30ml，直径<4cm,中线移位<5cm；"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"颈椎","c" :[
+		{"check":false,"val":"5","c":"完全性脊髓损伤（四肢瘫或截瘫）；C4以下骨折/脱位"},
+		{"check":false,"val":"4","c":"颈髓不完全损伤（残留部分感觉或运动功能）"},
+		{"check":false,"val":"3","c":"脊髓一过性神经损伤；椎体压缩重度压缩>20%；椎间盘破裂伴有神经根损伤"},
+		{"check":false,"val":"2","c":"颈椎椎体轻度压缩（<20%），棘突、横突骨折、椎间盘损伤（无神经根损伤）"},
+		{"check":false,"val":"1","c":"扭伤无骨折"}]
+	},	
+	{"check":false,"t":"喉声带","c" :[
+		{"check":false,"val":"5","c":"喉/咽横断毁损"},
+		{"check":false,"val":"4","c":"喉破裂伴有声带受损，咽部或咽喉壁穿孔"},
+		{"check":false,"val":"3","c":"破裂未横断/咽部挫伤血肿、撕裂伤；双侧声带损伤"},
+		{"check":false,"val":"2","c":"单侧挫伤"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"食道、气道","c" :[
+		{"check":false,"val":"5","c":"气管/食道横断或撕脱"},
+		{"check":false,"val":"4","c":"破裂未横断"},
+		{"check":false,"val":"3","c":"裂伤未穿孔"},
+		{"check":false,"val":"2","c":""},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"血管","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"颈总（内）动脉破裂    （失血>  20%）/内膜撕裂/创伤血栓形成"},
+		{"check":false,"val":"3","c":"颈内动脉内膜撕裂/破裂（失血量<20%）"},
+		{"check":false,"val":"2","c":"颈外动脉内膜撕裂、破裂（出血量<20%）/血栓形成，颈内静脉破裂（失血量<20%）"},
+		{"check":false,"val":"1","c":"颈外静脉轻度破裂（失血<20%）"}]
+	},
+	{"check":false,"t":"颅骨","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"颅骨凹陷性骨折>2cm，复杂性、粉碎性颅底骨折"},
+		{"check":false,"val":"3","c":"颅底骨折；粉碎性/开放性/凹陷（<2cm）性颅顶骨折"},
+		{"check":false,"val":"2","c":"单纯颅顶骨折"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"臂丛神经","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"完全性臂丛神经损伤"},
+		{"check":false,"val":"2","c":"不完全性臂丛神经损伤"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"神经根","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"多根神经根损伤"},
+		{"check":false,"val":"2","c":"单根神经根挫裂伤"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"甲状腺","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"裂伤"},
+		{"check":false,"val":"1","c":"挫伤"}]
+	},
+	{"check":false,"t":"颅神经","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"挫裂伤"},
+		{"check":false,"val":"1","c":""}]
+	}	
+]},
+{"img":"t_2.png","check":false,"t" :"面部","t_en" :"Face","c" :[
+	{"check":false,"t":"上颌骨","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"上颌骨Lefort III 型骨折伴失血量>20%"},
+		{"check":false,"val":"3","c":"上颌骨Lefort III型骨折"},
+		{"check":false,"val":"2","c":"上颌骨（包括上颌窦）Lefort I型Lefort II型骨折 "},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"眼眶","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"眼眶开放性/移位/粉碎性骨折"},
+		{"check":false,"val":"2","c":"眼眶闭合性骨折，颧骨骨折，颞颌关节脱位"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"眼球","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"眼撕脱/巩膜裂伤累及眼球"},
+		{"check":false,"val":"1","c":"角膜擦伤/玻璃体损伤/巩膜裂伤"}]
+	},
+	{"check":false,"t":"视神经","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"视神经裂伤"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"舌","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"广泛裂伤"},
+		{"check":false,"val":"1","c":"浅表裂伤"}]
+	},
+	{"check":false,"t":"鼻骨下颌骨","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"开放性/移位/粉碎性骨折"},
+		{"check":false,"val":"1","c":"鼻出血；鼻骨，下颌骨闭合性骨折"}]
+	},
+	{"check":false,"t":"齿","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":""},
+		{"check":false,"val":"1","c":"牙龈挫裂伤；撕裂伤；牙齿断裂，撕脱"}]
+	},
+	{"check":false,"t":"耳","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":""},
+		{"check":false,"val":"1","c":"耳道损伤（内耳/中耳/听骨链/鼓膜破裂"}]
+	}	
+]},
+{"img":"t_3.png","check":false,"t" :"胸部","t_en" :"Chest","c" :[
+	{"check":false,"t":"肋骨及胸骨","c" :[
+		{"check":false,"val":"5","c":"双侧连枷胸"},
+		{"check":false,"val":"4","c":"双侧肋骨均>三根骨折"},
+		{"check":false,"val":"3","c":">1根肋骨开放性/移位/粉碎性骨折；一侧>3根及另外一侧<3根肋骨骨折，胸廓稳定；单侧连枷胸； "},
+		{"check":false,"val":"2","c":"2-3根肋骨骨折或单根肋骨多处骨折；胸骨骨折"},
+		{"check":false,"val":"1","c":"单根肋骨骨折；胸壁擦伤；胸骨挫伤"}]
+	},
+	{"check":false,"t":"食道、气道","c" :[
+		{"check":false,"val":"5","c":"食道/主支气管复杂性破裂或横断；喉气管分离"},
+		{"check":false,"val":"4","c":"食道或支气管破裂穿孔，未完全断裂"},
+		{"check":false,"val":"3","c":"食道裂伤未穿孔，周径<50%;气管或主支气管挫伤/裂伤/主支气管以远部分破裂"},
+		{"check":false,"val":"2","c":"食道挫伤或胸导管裂伤"},
+		{"check":false,"val":"1","c":"主支气管挫伤（血肿）"}]
+	},	
+	{"check":false,"t":"脊柱脊髓","c" :[
+		{"check":false,"val":"5","c":"脊髓断裂或完全性损伤"},
+		{"check":false,"val":"4","c":"不完全性脊髓损伤"},
+		{"check":false,"val":"3","c":"胸椎脱位或椎板/椎弓根/关节突骨折，椎体压缩性骨折>1个椎体，高度 > 20%"},
+		{"check":false,"val":"2","c":"胸椎脱位或棘突或横突骨折；胸椎轻度压缩骨折（<20%）"},
+		{"check":false,"val":"1","c":"胸椎扭伤"}]
+	},	
+	{"check":false,"t":"血管","c" :[
+		{"check":false,"val":"5","c":"胸主动脉重度裂伤或裂伤累及主动脉根部或主动脉瓣；锁骨下/无名静脉/上下腔静脉裂伤伴有气体栓塞"},
+		{"check":false,"val":"4","c":"胸主动脉内膜撕裂，血管未破裂；血管破裂（失血<20%）;锁骨下/无名/肺动脉/上下腔静脉重度裂伤(失血>20%)"},
+		{"check":false,"val":"3","c":"无名/肺/锁骨下动静脉或上/下腔静脉胸段内膜撕裂/破裂（失血<20%）"},
+		{"check":false,"val":"2","c":"支气管/食道/肋间/乳内动脉/静脉破裂（失血<20%）；"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"心脏纵膈","c" :[
+		{"check":false,"val":"5","c":"心包裂伤，心脏疝出；心脏裂伤"},
+		{"check":false,"val":"4","c":"纵膈血肿"},
+		{"check":false,"val":"3","c":"纵膈气肿；心包填塞"},
+		{"check":false,"val":"2","c":"心包裂伤（穿刺伤）"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"肺脏","c" :[
+		{"check":false,"val":"5","c":"单侧/双侧裂伤伴有张力性气胸；大量气胸；双侧失血 > 20%"},
+		{"check":false,"val":"4","c":"双侧肺挫伤（失血> 20%）;双侧血气胸伴有张力性气胸，失血量>20%;张力性气胸"},
+		{"check":false,"val":"3","c":"单侧肺挫伤/裂伤；单侧血胸或气胸；轻度吸入性烧伤"},
+		{"check":false,"val":"2","c":"胸膜挫伤"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"膈肌","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"膈肌破裂伴膈疝"},
+		{"check":false,"val":"3","c":"膈肌破裂； "},
+		{"check":false,"val":"2","c":"膈肌挫伤（血肿）"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"乳房","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"乳房撕脱伤"},
+		{"check":false,"val":"1","c":""}]
+	}	
+]},
+{"img":"t_4.png","check":false,"t" :"腹部及盆腔","t_en" :"Abdomen and Pelvis","c" :[
+	{"check":false,"t":"脏器","c" :[
+		{"check":false,"val":"5","c":"重度裂伤伴组织缺失或严重污染（胰头/肝脏/脾脏/肾脏）； "},
+		{"check":false,"val":"4","c":"复杂性破裂：肝脏/脾脏/肾脏/胰脏、肠系膜广泛撕裂、胆囊破裂伴胆总管或肝总管裂伤/横断"},
+		{"check":false,"val":"3","c":"肝脏、胆囊、脾脏、肾脏、胰脏、胆囊广泛破裂/撕脱/胆囊裂伤/横断"},
+		{"check":false,"val":"2","c":"肝、脾、肾、胰挫伤；胆囊挫伤（血肿）及破裂未伤及胆囊、肠系膜撕裂伤（失血量<20%）"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"胃肠","c" :[
+		{"check":false,"val":"5","c":"十二指肠"},
+		{"check":false,"val":"4","c":"胃撕脱或复杂性破裂、十二指肠降部破裂>75%周径、累及壶腹部或胆总管下段、大/小肠横断或撕脱、"},
+		{"check":false,"val":"3","c":"胃十二指肠降部裂伤、穿孔；小肠/大肠/破裂穿孔未横断或挫伤未穿孔周径>50%；肠系膜重度损伤（失血量>20%）"},
+		{"check":false,"val":"2","c":"胃十二指肠挫伤、小肠、结肠、挫伤"},
+		{"check":false,"val":"1","c":"胃肠擦伤/挫伤/血肿/浅表裂伤"}]
+	},
+	{"check":false,"t":"直肠/会阴/肛<br>门/尿道/膀胱","c" :[
+		{"check":false,"val":"5","c":"直肠广泛破裂/撕脱/盆腔明显污染，"},
+		{"check":false,"val":"4","c":"膀胱穿孔破裂、尿道组织毁损、直肠穿孔延伸至会阴、"},
+		{"check":false,"val":"3","c":"直肠破裂穿孔未横断或挫伤未穿孔周径>50%；膀胱裂伤未穿孔、输尿管、尿道、肛门、会阴撕裂"},
+		{"check":false,"val":"2","c":"直肠、膀胱、输尿管、尿道挫伤；会阴撕裂伤；肛门撕裂伤（非全层）"},
+		{"check":false,"val":"1","c":"会阴擦伤、血尿"}]
+	},
+	{"check":false,"t":"男女生殖器","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"子宫裂伤（晚孕）、"},
+		{"check":false,"val":"3","c":"子宫破裂（中孕）、会阴、外阴、阴道、阴茎广泛撕裂;卵巢毁损、肾上腺重度毁损"},
+		{"check":false,"val":"2","c":"卵巢、子宫撕裂伤；阴囊、睾丸复杂性撕裂伤或撕脱；阴道、外阴、阴茎、"},
+		{"check":false,"val":"1","c":"阴道、阴唇、会阴、睾丸、阴茎擦伤、挫伤、血肿、浅表裂伤"}]
+	},
+	{"check":false,"t":"脊柱脊髓","c" :[
+		{"check":false,"val":"5","c":"完全性脊髓损伤；"},
+		{"check":false,"val":"4","c":"不完全性脊髓损伤"},
+		{"check":false,"val":"3","c":"腰椎滑脱或椎板、椎弓根、关节突骨折；椎体压缩性骨折>1个椎体或20%前缘高度；椎间盘脱出伴神经根损伤"},
+		{"check":false,"val":"2","c":"单侧小关节脱位（半脱位）；棘突或横突骨折；椎体压缩性骨折 (<20%)；椎间盘损伤（不伴有神经根损伤）"},
+		{"check":false,"val":"1","c":"腰扭伤"}]
+	},
+	{"check":false,"t":"神经根","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":">1个神经根损伤"},
+		{"check":false,"val":"2","c":"单根神经根损伤"},
+		{"check":false,"val":"1","c":""}]
+	},	
+	{"check":false,"t":"血管","c" :[
+		{"check":false,"val":"5","c":"腹主动脉、腹腔动脉破裂（失血量>20%）"},
+		{"check":false,"val":"4","c":"腹主动脉内膜撕裂、破裂（失血量<20%）、髂动脉（总、内、外）、下腔静脉破裂（失血量>20%）"},
+		{"check":false,"val":"3","c":"腹腔动脉/髂动脉/总、内、外内膜撕裂未破裂或破裂出血<20%/下腔静脉破裂出血破裂出血>20%、"},
+		{"check":false,"val":"2","c":"腹腔动脉/髂动脉/总、内、外内膜撕裂未破裂或破裂出血<20%，下腔静脉破裂出血<20%"},
+		{"check":false,"val":"1","c":""}]
+	}	
+]},
+{"img":"t_5.png","check":false,"t" :"四肢及骨盆","t_en" :"Extremities and Pelvis","c" :[
+	{"check":false,"t":"骨盆","c" :[
+		{"check":false,"val":"5","c":"开放性/移位/粉碎性骨盆骨折（失血量>20%）"},
+		{"check":false,"val":"4","c":"骨盆骨折移位，伴血管破裂或巨大腹膜后血肿的开放性/移位性/粉碎性骨折（失血<20%）;膝关节以上完全性离断"},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"耻骨支骨折；骨盆单纯性骨折;耻骨联合分离"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"四肢骨","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"股骨骨折；膝关节以下的肢体离断、脱套伤、部分或广泛、毁损性挤压伤； "},
+		{"check":false,"val":"2","c":"肱骨、桡骨、尺骨、胫骨、腓骨、锁骨、肩胛骨、腕骨、掌骨、跟骨、跗骨、跖骨骨折；肘关节、肩关节、肩锁关节、髋关节、膝关节脱位；指、趾离断"},
+		{"check":false,"val":"1","c":"腕骨、指骨、趾骨骨折；肩锁关节、肩肘关节、指、腕、髋、踝、趾关节扭伤"}]
+	},
+	{"check":false,"t":"神经","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"坐骨神经损伤/断裂"},
+		{"check":false,"val":"2","c":"单根/多根神经损伤（伴运动功能障碍）"},
+		{"check":false,"val":"1","c":"神经挫伤"}]
+	},
+	{"check":false,"t":"血管","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":"股动脉内膜撕裂/破裂（失血量<20%）"},
+		{"check":false,"val":"2","c":"尺桡血管、肱动脉、腘静脉内膜裂伤/轻度裂伤（失血量<20%）"},
+		{"check":false,"val":"1","c":""}]
+	},
+	{"check":false,"t":"肌肉肌腱","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"严重肌肉、肌腱撕裂伤；半月板撕裂； "},
+		{"check":false,"val":"1","c":""}]
+	}	
+]},
+{"img":"t_6.png","check":false,"t" :"体表","t_en" :"Body surface","c" :[
+	{"check":false,"t":"烧伤面积","c" :[
+		{"check":false,"val":"5","c":"II度或III度烧伤面积40-89%"},
+		{"check":false,"val":"4","c":"II度或III度烧伤面积30-39%"},
+		{"check":false,"val":"3","c":"II度或III度烧伤面积20-29%"},
+		{"check":false,"val":"2","c":"II度或III度烧伤面积10-19%"},
+		{"check":false,"val":"1","c":"I度0-100%；II度III度烧伤面积<10%;"}]
+	},
+	{"check":false,"t":"脱套伤面积","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":"脱套伤面积30-39%"},
+		{"check":false,"val":"3","c":"脱套伤面积20-29%；(失血量>20%)"},
+		{"check":false,"val":"2","c":"脱套伤面积10-19%（失血量<20%）"},
+		{"check":false,"val":"1","c":"脱套伤面积<10%"}]
+	},
+	{"check":false,"t":"四肢","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"擦伤/挫伤面积>25cm2；裂伤> 5cm;"},
+		{"check":false,"val":"1","c":"擦伤/挫伤面积<25cm2裂伤< 5cm;"}]
+	},
+	{"check":false,"t":"躯干部","c" :[
+		{"check":false,"val":"5","c":""},
+		{"check":false,"val":"4","c":""},
+		{"check":false,"val":"3","c":""},
+		{"check":false,"val":"2","c":"擦伤/挫伤>50cm2，裂伤>10cm"},
+		{"check":false,"val":"1","c":"擦伤/挫伤<50cm2；裂伤<10cm"}]
+	}	
+]}
+];
---
+++ XXX_ISS_WEB/sql/init.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS t_user_info;
+CREATE TABLE t_user_info (
+  id int(10) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  name varchar(100) DEFAULT NULL COMMENT '用户名称',
+  sex varchar(100) DEFAULT NULL COMMENT '性别',
+  age int(10) DEFAULT NULL COMMENT '年龄', 
+  com varchar(100) DEFAULT NULL COMMENT '公司（医院）',
+  dept varchar(100) DEFAULT NULL COMMENT '部门（科室）',
+  post varchar(100) DEFAULT NULL COMMENT '岗位（职称）',
+  tel varchar(100) DEFAULT NULL COMMENT '电话',
+  email varchar(100) DEFAULT NULL COMMENT '电子邮箱',
+  phone varchar(20) DEFAULT NULL COMMENT '手机号',
+  user_type varchar(100) DEFAULT NULL COMMENT '用户类型',
+  user_code varchar(100) DEFAULT NULL COMMENT '用户识别码可以是身份证、手机号等',
+  weixin_open_id  varchar(100) DEFAULT NULL COMMENT '微信用户唯一标识符',  
+  weixin_id varchar(30) NOT NULL COMMENT '微信公众账号ID',
+  create_time  timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (id)
+)
