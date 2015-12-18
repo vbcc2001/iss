b6485ad630fa20bf3275400f00aeb616d980c11f
vbcc2001 <vbcc2001@gmail.com>
2015-12-18 13:30:40 +0800

    第十四版

--- XXX_ISS_WEB/src/com/xxx/iss/http/action/MenuAction.java
+++ XXX_ISS_WEB/src/com/xxx/iss/http/action/MenuAction.java
@@ -3,7 +3,8 @@ package com.xxx.iss.http.action;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
@@ -20,22 +21,28 @@ import com.google.gson.reflect.TypeToken;
  */
 public class MenuAction extends BaseAction {
 
-	public ActionResult doDefault() {
+	public ActionResult doDefault() throws UnsupportedEncodingException {
 		String secret = "9e6ec87af00ea0bc3039b1a601eb7a22";
 		String appid = "wx91f0fb6fe746a4bc";
 		String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret;
-		String result = this.sendPost(url, "");
+		String result = this.sendPost(url, "".getBytes());
 		logger.info(result);
 		Gson gson = new GsonBuilder().serializeNulls().create();
 		@SuppressWarnings("unchecked")
 		Map<String,String> params = (Map<String,String>)gson.fromJson(result, new TypeToken<Map<String,String>>() {}.getType());   
 		String access_token =params.get("access_token");
+		logger.info(access_token);
 		url ="https://api.weixin.qq.com/cgi-bin/menu/create?access_token="+access_token;
 		String  button_url = "https://open.weixin.qq.com/connect/oauth2/authorize"
 				+ "?appid=wx91f0fb6fe746a4bc&redirect_uri=http%3A%2F%2Fwww.m-trauma.com/action/iss/http/action/OpenIdAction"
 				+ "&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect";
-		String  body= " {\"button\":[{ \"type\":\"view\",\"name\":\"ISS评分\",\"url\":\""+button_url+"\"},] }";
-		result = this.sendPost(url, body);
+		String  body= " {\"button\":[{ \"type\":\"view\",\"name\":\"ISS评分\",\"url\":\""+button_url+"\"}] }";
+		
+		//{"button":[{ "type":"view","name":"ISS评分","url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx91f0fb6fe746a4bc&redirect_uri=http%3A%2F%2Fwww.m-trauma.com/action/iss/http/action/OpenIdAction&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirec"}] }
+		//[web] 2015-12-18 10:46:12 [INFO ] (MenuAction:28) {"access_token":"9jax85VapdeYS3BtzROfOCJptgWA5-SdAjjm0lerEOHs1oYh2sBb7j6ET3t8M51pv89zLp2ujb9aceDlJe9mH94ju3bQTLsf85eByNZZ2KcHRYcAGAAKR","expires_in":7200}
+		
+		result = this.sendPost(url, body.getBytes("UTF-8"));
+		
 		logger.info(result);
 		this.getWriter().print(result);
 		return null;
@@ -49,8 +56,8 @@ public class MenuAction extends BaseAction {
      *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
      * @return 所代表远程资源的响应结果
      */
-    public String sendPost(String url, String param) {
-        PrintWriter out = null;
+    public String sendPost(String url, byte[] param) {
+    	OutputStream os = null;
         BufferedReader in = null;
         String result = "";
         try {
@@ -64,12 +71,10 @@ public class MenuAction extends BaseAction {
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
+            os = conn.getOutputStream();
+            os.write(param);//传入参数    
+            os.flush();
+            os.close();
             // 定义BufferedReader输入流来读取URL的响应
             in = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()));
@@ -84,8 +89,8 @@ public class MenuAction extends BaseAction {
         //使用finally块来关闭输出流、输入流
         finally{
             try{
-                if(out!=null){
-                    out.close();
+                if(os!=null){
+                	os.close();
                 }
                 if(in!=null){
                     in.close();
--- XXX_ISS_WEB/src/com/xxx/iss/http/action/WeiXinAction.java
+++ XXX_ISS_WEB/src/com/xxx/iss/http/action/WeiXinAction.java
@@ -9,6 +9,39 @@ package com.xxx.iss.http.action;
 public class WeiXinAction extends BaseAction {
 
 	public ActionResult doDefault() {
+			/*
+			String  signature = getStrParameter("interestId");
+			String  timestamp = getStrParameter("timestamp");
+			String  nonce = getStrParameter("nonce");
+			String  echostr = getStrParameter("echostr");
+			
+			String token = "ytzq";  
+			
+			System.out.println(signature);
+			System.out.println(timestamp);
+			System.out.println(nonce);
+			System.out.println(echostr);
+			
+			//对请求参数和自己的token进行排序，并连接排序后的结果为一个字符串        
+			String[] strSet = new String[]{token, timestamp, nonce};       
+			java.util.Arrays.sort(strSet);        
+			String total = "";        
+			for (String string : strSet) { 
+				total = total + string;        
+			} 
+			//SHA-1加密实例        
+			MessageDigest sha1 = MessageDigest.getInstance("SHA-1");        
+			sha1.update(total.getBytes());        
+			byte[] codedBytes = sha1.digest();        
+			String codedString = new BigInteger(1, codedBytes).toString(16);
+			//将加密后的字节数组转换成字符串。参见http://hi.baidu.com/aotori/item/c94813c4f15caa60f6c95d4a        
+			if (codedString.equals(signature)) { 
+				this.writeMessageToPage(echostr);
+			}else{
+				this.writeMessageToPage(echostr);
+			}
+			return null;
+			 */
 			String echostr = getRequest().getParameter("echostr");
 			this.getWriter().print(echostr);
 			return null;
