064b98735c8d1c7bd67a6707d866b8d88e34d92f
vbcc2001 <vbcc2001@gmail.com>
2015-12-29 18:05:52 +0800

    26版

--- XXX_ISS_WEB/src/com/xxx/xls/action/Upload.java
+++ XXX_ISS_WEB/src/com/xxx/xls/action/Upload.java
@@ -1,9 +1,14 @@
 package com.xxx.xls.action;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -11,11 +16,13 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.xxx.iss.http.action.ActionResult;
 import com.xxx.iss.http.action.BaseAction;
 
 /**
- * 微信认证
+ * 文件上传
  * @author 门士松  20121031
  * @version 1.0
  * @since
@@ -24,6 +31,7 @@ public class Upload extends BaseAction {
 
 	public ActionResult doDefault() throws Exception {
 		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+		Map<String,Object> result = new HashMap<String,Object>();
 		if(isMultipart){
 			DiskFileItemFactory factory = new DiskFileItemFactory();
 			ServletContext servletContext = this.getSession().getServletContext();
@@ -41,15 +49,15 @@ public class Upload extends BaseAction {
 			        //processUploadedFile(item);
 			    	//String fieldName = item.getFieldName();
 			        String fileName = item.getName();
-			        this.getWriter().print("{\"name\":\""+fileName+"\"}");
+			        result.put("name",fileName );
 			        //String contentType = item.getContentType();
 			        //boolean isInMemory = item.isInMemory();
 			        //long sizeInBytes = item.getSize();
 			        // Process a file upload
 			        boolean writeToFile =true;
 			        if (writeToFile) {
-			            //File uploadedFile = new File("D:/iss/XXX_ISS_WEB/WebRoot/xls/csv/1.csv");
-			            File uploadedFile = new File("C:/XXX_ISS_DEV/XXX_ISS_WEB/WebRoot/xls/csv/1.csv");
+			            File uploadedFile = new File("D:/iss/XXX_ISS_WEB/WebRoot/xls/csv/1.csv");
+			            //File uploadedFile = new File("C:/XXX_ISS_DEV/XXX_ISS_WEB/WebRoot/xls/csv/1.csv");
 			            item.write(uploadedFile);
 			        } else {
 			            //InputStream uploadedStream = item.getInputStream();
@@ -59,7 +67,27 @@ public class Upload extends BaseAction {
 			    }
 			}
 		}
-		
+		//String pathname  = "C:/XXX_ISS_DEV/XXX_ISS_WEB/WebRoot/xls/csv/1.csv";
+		String pathname  = "D:/iss/XXX_ISS_WEB/WebRoot/xls/csv/1.csv";
+		File filename = new File(pathname); // 要读取以上路径的input。txt文件
+		// 建立一个输入流对象reader
+		InputStreamReader reader = new InputStreamReader(new FileInputStream(filename));
+		BufferedReader br =null;
+		try {
+			br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
+			String title = br.readLine(); 
+			String[] titles =  title.split(",");
+			result.put("titles", titles);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if(br!=null) {
+				br.close();
+			}
+		}
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		String json =  gson.toJson(result);
+		this.getWriter().print( json);
 		return null;
-	}   
+	}
 }
