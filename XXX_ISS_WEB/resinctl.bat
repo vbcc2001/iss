820340decd8fdd9a639c0ac5a3039315076abd16
vbcc2001 <vbcc2001@gmail.com>
2017-02-08 22:50:43 +0800

    2017年2月8日部署更新

--- XXX_ISS_WEB/resinctl.bat
+++ XXX_ISS_WEB/resinctl.bat
@@ -1,7 +1,7 @@
 @echo off
-set JAVA_HOME=D:\YTZQ_Web_Dev\jdk1.8.0_40
-set RESIN_HOME=D:\YTZQ_Web_Dev\resin-pro-3.1.12
-set APP_HOME=D:\iss\XXX_ISS_WEB
+set JAVA_HOME=C:\iss\jdk1.8.0_101
+set RESIN_HOME=C:\iss\resin-pro-3.1.12
+set APP_HOME=C:\iss\XXX_ISS_WEB
 set APP_NAME=XXX_ISS_WEB
 title %APP_NAME%
 %JAVA_HOME%\bin\java -Dfile.encoding=GBK -Dapp.name=%APP_NAME% -Dapp.home=%APP_HOME% -jar %RESIN_HOME%\lib\resin.jar -resin-home %RESIN_HOME% -conf %APP_HOME%\resin.xml -server %APP_NAME% 
